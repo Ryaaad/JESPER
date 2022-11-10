57bf0eb5f31b42e69f1add13826ca92b7e4ce4b6
Ryaaad <ryadallali0@gmail.com>
2022-11-10 18:02:35 +0100

    fixing the display on App

--- GamersPlatform/src/App.tsx
+++ GamersPlatform/src/App.tsx
@@ -2,13 +2,14 @@ import Hero2 from "./Componants/Home/Hero2"
 import Home0 from "./Componants/Home/Home"
 import Navbar1 from "./Componants/Navbar/Navbar1"
 import Footer from "./Componants/footer/footer"
+
 function App() {
  
 
-  return (
-    <div className="flex justify-center relative bg-[#07080d] font-[Montserrat,sans-serif]  ">
-   <Navbar1></Navbar1>
+  return (<div  className="bg-[#07070d]">
+  <Navbar1></Navbar1>
   </div>
+ 
   )
 }
 
